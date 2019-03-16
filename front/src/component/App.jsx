7bc13f7f05ece3a1f37ab405d4682cedd4831f41
t0lia <t0lia@yandex.ru>
2019-03-16 08:48:43 +0300

    add routing

--- front/src/component/App.jsx
+++ front/src/component/App.jsx
@@ -1,23 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
 import Review from "./Review";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 
 class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <ul>
-                    <li> review</li>
-                    <li> plan</li>
-                    <li> write</li>
+            <Router>
+                <div className="App">
+                    <Header/>
 
-
-                </ul>
-                <Review/>
-                <Plan/>
-                <Write/>
-            </div>
+                    <Route exact path="/" component={Review}/>
+                    <Route path="/plan" component={Plan}/>
+                    <Route path="/add" component={Add}/>
+                </div>
+            </Router>
         );
     }
 }
@@ -32,6 +30,23 @@ function Add() {
     return (
         <div><h3>Add</h3></div>
     )
+}
+
+
+function Header() {
+    return (
+        <ul>
+            <li>
+                <Link to="/">Review</Link>
+            </li>
+            <li>
+                <Link to="/plan">Plan</Link>
+            </li>
+            <li>
+                <Link to="/add">Add</Link>
+            </li>
+        </ul>
+    );
 }
 
 export default App;
